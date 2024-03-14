988f0f21c1f37203ede72cf0f22ddc3b8e1485d6
Robdsjr007 <robson.santana15@etec.sp.gov.br>
2024-03-14 02:43:25 -0300

    falta o back end e integração com a base de dados

---
+++ view/screens/list.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Lista de usuários</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <a href='../index.html'>Voltar</a>
+            <h1>Lista de Usuários</h1>
+        </div>
+        <div class="conteudo">
+            <table>
+                <tr>
+                    <th>Id</th>
+                    <th>Email</th>
+                    <th>Senha</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Maria Anders</td>
+                    <td>1824Maria23@</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Francisco Almeida</td>
+                    <td>francisco090287</td>
+                </tr>
+                <tr>
+                    <td>Centro comercial Moctezuma</td>
+                    <td>Francisco Chang</td>
+                    <td>Mexico</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
